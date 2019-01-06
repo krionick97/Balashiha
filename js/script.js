53fc1f5179daf2d3eb05568233ee1e935bb1874e
krionick97 <mistermers8@gmail.com>
2019-01-06 21:40:55 +0300

    section-slider

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
-  $('#phone').mask('+7 (999) 999-99-99');
+  $('.input-phone > input').mask('+7 (999) 999-99-99');
+  $('.photo-slider__slider').slick({
+    dots: true,
+    infinite: true,
+    // speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: '<div class="arrow arrow-prev"></div>',
+    prevArrow: '<div class="arrow arrow-next"></div>',
+    dotsClass: 'dots'
+    // dots: 'dots'
+  });
 });
